108e46117316c00597e19c8b4808e83dbd455fc2
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-30 21:11:31 +0530

    Create Question-05.C

---
+++ Question-05.C
@@ -0,0 +1,35 @@
+/*Write a function to find the first occurrence of adjacent duplicate values in the array.
+Function has to return the value of the element.*/
+
+#include<stdio.h>
+
+int duplicate (int a[] , int n ) ;
+
+int duplicate (int a[] , int n ) 
+{
+        int i ; 
+
+        printf("\n Enter %d values of a[] ",n);
+        for(i=0 ; i<n ; i++)
+                scanf("%d",&a[i]);
+
+        
+        for(i=0 ; i<=n-1 ; i++)
+        {
+                
+                if( a[i]==a[i+1] )
+                        return a[i] ;
+                else 
+                        return 0 ;
+                
+        }
+}
+
+int main ()
+{
+        int b[10] ;
+        int x = duplicate(b,10);
+
+        printf("1st occurrence of adjacent duplicate value is %d ",x);
+}
+
