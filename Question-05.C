1606d7a42ec5b57296cb87d62c6df3beaf071356
Nilufa Yasmin <116085061+MissNilufaYasmin@users.noreply.github.com>
2023-06-30 22:02:11 +0530

    Update Question-05.C

--- Question-05.C
+++ Question-05.C
@@ -16,12 +16,8 @@ int duplicate (int a[] , int n )
         
         for(i=0 ; i<=n-1 ; i++)
         {
-                
                 if( a[i]==a[i+1] )
                         return a[i] ;
-                else 
-                        return 0 ;
-                
         }
 }
 
